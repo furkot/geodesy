228bb9e905b858a6c7ae5583d7c07e9dd07b3d63
Damian Krzeminski <pirxpilot@furkot.com>
2026-08-02 21:12:48 -0600

    adjust default radius to earth radius in meters
    see:
    https://developers.google.com/maps/documentation/javascript/reference/geometry#spherical

--- test/geodesy.js
+++ test/geodesy.js
@@ -1,6 +1,8 @@
 import test from 'node:test';
 import { alongTrackDistance, crossTrackDistance, distance, initialBearing, intermediatePoint } from '../lib/geodesy.js';
 
+const R = 6371e3; // approximate Earth radius in metres
+
 const cambridge = [0.119, 52.205];
 const paris = [2.351, 48.857];
 const lax = [-118.4, 33.95];
@@ -11,7 +13,8 @@ const dunham = [-0.7972, 53.2611];
 const partney = [0.1334, 53.1887];
 
 test('distance', t => {
-  t.assert.equal(distance(cambridge, paris).toFixed(), '404279');
+  t.assert.equal(distance(cambridge, paris).toFixed(), '404732');
+  t.assert.equal(distance(cambridge, paris, R).toFixed(), '404279');
   t.assert.equal(distance(cambridge, paris, 3959).toFixed(1), '251.2');
 });
 
@@ -19,20 +22,19 @@ test('initialBearing', t => {
   t.assert.ok(Number.isNaN(initialBearing(cambridge, cambridge)), 'should be NaN when from and to are the same');
 
   t.assert.equal(initialBearing(lax, jfk).toPrecision(2), '66');
-
   t.assert.equal(initialBearing(paris, cambridge).toFixed(1), '337.9');
-
   t.assert.equal(initialBearing(cambridge, paris).toFixed(1), '156.2');
 });
 
 test('alongTrackDistance', t => {
-  t.assert.equal(alongTrackDistance(dunham, bradwell, partney).toPrecision(4), '6.233e+4');
+  t.assert.equal(alongTrackDistance(dunham, bradwell, partney).toPrecision(4), '6.240e+4');
+  t.assert.equal(alongTrackDistance(dunham, bradwell, partney, R).toPrecision(4), '6.233e+4');
 
-  t.assert.equal(alongTrackDistance([1, 1], [0, 0], [2, 0]).toPrecision(4), '1.112e+5');
-  t.assert.equal(alongTrackDistance([1, -1], [0, 0], [2, 0]).toPrecision(4), '1.112e+5');
-  t.assert.equal(alongTrackDistance([-1, -1], [0, 0], [2, 0]).toPrecision(4), '-1.112e+5');
-  t.assert.equal(alongTrackDistance([-1, 1], [0, 0], [2, 0]).toPrecision(4), '-1.112e+5');
-  t.assert.equal(alongTrackDistance([0, 10], [0, 10], [2, 0]), 0);
+  t.assert.equal(alongTrackDistance([1, 1], [0, 0], [2, 0], R).toPrecision(4), '1.112e+5');
+  t.assert.equal(alongTrackDistance([1, -1], [0, 0], [2, 0], R).toPrecision(4), '1.112e+5');
+  t.assert.equal(alongTrackDistance([-1, -1], [0, 0], [2, 0], R).toPrecision(4), '-1.112e+5');
+  t.assert.equal(alongTrackDistance([-1, 1], [0, 0], [2, 0], R).toPrecision(4), '-1.112e+5');
+  t.assert.equal(alongTrackDistance([0, 10], [0, 10], [2, 0], R), 0);
 
   const r = (180 * 60) / Math.PI; // earth radius in nautical miles
   const d = [-116.5, 34.5];
@@ -41,15 +43,16 @@ test('alongTrackDistance', t => {
 });
 
 test('crossTrackDistance', t => {
-  t.assert.equal(crossTrackDistance(dunham, bradwell, partney).toPrecision(4), '-307.5');
+  t.assert.equal(crossTrackDistance(dunham, bradwell, partney).toPrecision(4), '-307.9');
+  t.assert.equal(crossTrackDistance(dunham, bradwell, partney, R).toPrecision(4), '-307.5');
 
-  t.assert.equal(crossTrackDistance([1, 10], [0, 0], [2, 0]).toPrecision(4), '-1.112e+6');
+  t.assert.equal(crossTrackDistance([1, 10], [0, 0], [2, 0], R).toPrecision(4), '-1.112e+6');
 
-  t.assert.equal(crossTrackDistance([1, 1], [0, 0], [2, 0]).toPrecision(4), '-1.112e+5');
-  t.assert.equal(crossTrackDistance([1, -1], [0, 0], [2, 0]).toPrecision(4), '1.112e+5');
-  t.assert.equal(crossTrackDistance([-1, -1], [0, 0], [2, 0]).toPrecision(4), '1.112e+5');
-  t.assert.equal(crossTrackDistance([-1, 1], [0, 0], [2, 0]).toPrecision(4), '-1.112e+5');
-  t.assert.equal(crossTrackDistance([0, 10], [0, 10], [2, 0]), 0);
+  t.assert.equal(crossTrackDistance([1, 1], [0, 0], [2, 0], R).toPrecision(4), '-1.112e+5');
+  t.assert.equal(crossTrackDistance([1, -1], [0, 0], [2, 0], R).toPrecision(4), '1.112e+5');
+  t.assert.equal(crossTrackDistance([-1, -1], [0, 0], [2, 0], R).toPrecision(4), '1.112e+5');
+  t.assert.equal(crossTrackDistance([-1, 1], [0, 0], [2, 0], R).toPrecision(4), '-1.112e+5');
+  t.assert.equal(crossTrackDistance([0, 10], [0, 10], [2, 0], R), 0);
 
   const r = (180 * 60) / Math.PI; // earth radius in nautical miles
   const d = [-116.5, 34.5];
